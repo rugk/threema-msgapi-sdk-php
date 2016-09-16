2991995ce1fa0769a9146a7411898291a6d7bc06
rugk <rugk@posteo.de>
2016-09-16 20:25:18 +0200

    PHP env fix

--- scripts/buildLibsodiumPhp.sh
+++ scripts/buildLibsodiumPhp.sh
@@ -7,6 +7,14 @@ if [ "$LIBSODIUM" == false ]; then exit 0; fi
 # install PHP extension
 pecl install libsodium
 
+# install in missing PHP version envoriments
+
+# enable extension
+echo "extension=libsodium.so" > libsodium.ini
+phpenv config-add libsodium.ini
+# or php5enmod libsodium (not tested)
+# or echo "extension=<extension>.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
 # verify libsodium version
 LIBSODIUMVER=$( php -r 'echo \Sodium\version_string();' )
 echo "Installed libsodium version: $LIBSODIUMVER"
