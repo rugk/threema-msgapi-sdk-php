7ec9ae976829213a9470a720e3fe9ace391416b5
rugk <rugk@posteo.de>
2016-10-06 18:44:09 +0200

    Add note for param
    Without this note it is not clear which $threemaId is meant. This makes it clear and prevents things such as #41.

--- source/Threema/MsgApi/Helpers/E2EHelper.php
+++ source/Threema/MsgApi/Helpers/E2EHelper.php
@@ -186,7 +186,7 @@ class E2EHelper {
 	 * Note: This does not check the MAC before, which you should always do when
 	 * you want to use this in your own application! Use {@link checkMac()} for doing so.
 	 *
-	 * @param string $threemaId
+	 * @param string $threemaId The sender ID (= the ID the message came from)
 	 * @param string $messageId
 	 * @param string $box box as binary string
 	 * @param string $nonce nonce as binary string
